50f71b8b65f87032d0a6abe41f48fb37602635f5
tinsaye <abye@studserv.uni-leipzig.de>
2017-10-18 11:49:53 +0200

    add to git

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <button is="google-cast-button"></button>
+        <div id="msg">HI
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default App;
